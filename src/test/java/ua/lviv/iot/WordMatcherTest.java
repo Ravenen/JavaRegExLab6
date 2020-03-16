7c2002bc66ddc11f00abecbd224e53e4a2c04f13
Raven <redstoyningbros@gmail.com>
2020-03-16 19:36:33 +0200

    Fixed formatting

--- src/test/java/ua/lviv/iot/WordMatcherTest.java
+++ src/test/java/ua/lviv/iot/WordMatcherTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import ua.lviv.iot.match.WordMatcher;
 
 class WordMatcherTest {
-  
+
   @BeforeEach
   void setUp() {
   }
@@ -21,34 +21,34 @@ class WordMatcherTest {
   void testDeleteWordsStartingWithConsonantByLength2() {
     int inputLength = 2;
     String inputString = "What I supposed to do? I have to delete what? Ok, here is test string.";
-    
+
     String input = inputString + "\n" + inputLength;
     InputStream in = new ByteArrayInputStream(input.getBytes());
     System.setIn(in);
-    
+
     try (Scanner scanner = new Scanner(System.in);) {
       String stringToCheck = scanner.nextLine();
       int lengthOfWord = scanner.nextInt();
-      
+
       String actual = WordMatcher.deleteWordsStartingWithConsonantByLength(stringToCheck, lengthOfWord);
       String expected = "What I supposed  ? I have  delete what? Ok, here is test string.";
       assertEquals(expected, actual);
     }
   }
-  
+
   @Test
   void testDeleteWordsStartingWithConsonantByLength4() {
     int inputLength = 4;
     String inputString = "What I supposed to do? I have to delete what? Ok, here is test string.";
-    
+
     String input = inputString + "\n" + inputLength;
     InputStream in = new ByteArrayInputStream(input.getBytes());
     System.setIn(in);
-    
+
     try (Scanner scanner = new Scanner(System.in);) {
       String stringToCheck = scanner.nextLine();
       int lengthOfWord = scanner.nextInt();
-      
+
       String actual = WordMatcher.deleteWordsStartingWithConsonantByLength(stringToCheck, lengthOfWord);
       String expected = " I supposed to do? I  to delete ? Ok,  is  string.";
       assertEquals(expected, actual);
